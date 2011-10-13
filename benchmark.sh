95d872581eea3b7ef05e01f3f793cca58fbfc97e
Miles Sabin <miles@milessabin.com>
2011-10-13 06:39:57 +0100

    WIP.

--- benchmark.sh
+++ benchmark.sh
@@ -2,7 +2,7 @@
 
 . ./env.sh
 
-${SCALATRUNK}/bin/scala -cp bin fibonacci.FibBench fibonacci.ScalaStream --benchmark 1000000
-${SCALATRUNK}/bin/scala -cp bin fibonacci.FibBench fibonacci.ScalaGenerator --benchmark 10000000
-${SCALATRUNK}/bin/scala -cp bin fibonacci.FibBench fibonacci.JavaThread --benchmark 100000
-${SCALATRUNK}/bin/scala -cp bin fibonacci.FibBench fibonacci.JavaCoroutine --benchmark 10000000
+${SCALATRUNK}/bin/scala -cp bin benchmarks.FibBench benchmarks.ScalaStream --benchmark 1000000
+${SCALATRUNK}/bin/scala -cp bin benchmarks.FibBench benchmarks.ScalaGenerator --benchmark 10000000
+${SCALATRUNK}/bin/scala -cp bin benchmarks.FibBench benchmarks.JavaThread --benchmark 100000
+${SCALATRUNK}/bin/scala -cp bin benchmarks.FibBench benchmarks.JavaCoroutine --benchmark 10000000
--- footprint.sh
+++ footprint.sh
@@ -4,7 +4,7 @@
 
 ulimit -v 4194304 -n 4096 -u 63778
 
-${SCALATRUNK}/bin/scala -J-Xmx1G -cp bin fibonacci.FibBench fibonacci.ScalaStream --footprint
-${SCALATRUNK}/bin/scala -J-Xmx1G -cp bin fibonacci.FibBench fibonacci.ScalaGenerator --footprint
-${SCALATRUNK}/bin/scala -J-Xmx1G -J-Xss104k -cp bin fibonacci.FibBench fibonacci.JavaThread --footprint
-${SCALATRUNK}/bin/scala -J-Xmx3G -cp bin fibonacci.FibBench fibonacci.JavaCoroutine --footprint -v
+${SCALATRUNK}/bin/scala -J-Xmx1G -cp bin benchmarks.FibBench benchmarks.ScalaStream --footprint
+${SCALATRUNK}/bin/scala -J-Xmx1G -cp bin benchmarks.FibBench benchmarks.ScalaGenerator --footprint
+${SCALATRUNK}/bin/scala -J-Xmx1G -J-Xss104k -cp bin benchmarks.FibBench benchmarks.JavaThread --footprint
+${SCALATRUNK}/bin/scala -J-Xmx3G -cp bin benchmarks.FibBench benchmarks.JavaCoroutine --footprint -v
